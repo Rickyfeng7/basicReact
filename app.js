de0e124eb7bad8b04c498ce60fc274e60c705618
Ricky Feng <34487908+Rickyfeng7@users.noreply.github.com>
2018-05-21 08:50:08 -0700

    sort and structue

--- app.js
+++ app.js
@@ -16,24 +16,24 @@
 
                 //name is a sigle arguement
                 //to pass more (name, age)
-const printMyName = (name) =>{
-    console.log(name)
-}
+// const printMyName = (name) =>{
+//     console.log(name)
+// }
 
-// to add more printMyName("ricky", 25)
-printMyName("ricky")
+// // to add more printMyName("ricky", 25)
+// printMyName("ricky")
 
 
-// more lines of code to multiply by 2 
-const multiples = (number) => {
-    return number * 2
-}
+// // more lines of code to multiply by 2 
+// const multiples = (number) => {
+//     return number * 2
+// }
 
-console.log(multiples(2))
-// single line coding that multiples arguments
-const multiply = (number) => number * 2
+// console.log(multiples(2))
+// // single line coding that multiples arguments
+// const multiply = (number) => number * 2
 
-console.log(multiply(2))
+// console.log(multiply(2))
 
 //---// different ways to import files 
 // const person = {
@@ -95,3 +95,100 @@ console.log(multiply(2))
 
 // // can only inherat twice in js 
 
+                                        //------------------------// ES7
+
+                                        // class Human {
+
+                                        //     gender = 'male';
+                                        
+                                        //   printGender = () => {
+                                        //     console.log(this.gender)
+                                        //   }
+                                        // }
+
+
+                                        // class Person extends Human {
+                                        //   // name of method with ()
+
+                                        //     name = 'ricky';
+                                        //     gender = 'female';
+                                        
+                                        //   printMyName = () => {
+                                        //     console.log(this.name)
+                                        //   }
+                                        // }
+
+                                        // spread / rest operator === ...
+// // spread - used to split up array eleemnts Or object properties
+// const newArray = [... oldArray, 1,2]
+// const newObject = {...oldObject, newProp:5}
+
+// // rest - used to merge a list of function arguments into an array
+// function sortArgs(...args){
+//   return args.sort()
+// }
+
+
+// const numbers = [1,2,3]
+// // if no ... numbers would = [[1,2,3],4,5]
+// const newNumbers = [numbers, 4, 5]
+// right now it console.logs [1,2,3,4,5]
+// console.log(newNumbers) 
+// const person = {
+//   name: 'Ricky'
+// };
+
+// const newPerson = {
+//   ... person,
+//   age: 25
+// }
+// console.log(newPerson);
+// const filter = (...chicken) => {
+//     return chicken.filter(el => el === 1);
+//   }
+//   console.log(filter(1,2,3))
+
+// Destructuring - easily extract array elements or object properties and store them in variables
+// Array Destructuring
+// [a,b] = ['hello','Ricky']
+// console.log(a)//hello
+// console.log(b)//Ricky
+// object Destructuring
+// {name} ={name:'ricky', age:25}
+// console.log(name)//ricky
+// console.log(age)// undefined
+//------------------------
+// array destructuring
+// const numbers = [1, 2, 3];
+// //console.log(1,2)
+// // [num1, num2] = numbers;
+// // console.log(num1, num2;
+// //console.log(1,3)
+// [num1, , num3] = numbers;
+// console.log(num1, num3)
+
+// conse number = 1;
+// const num2 = number;
+
+// console.log(num2)
+
+// const person = {
+//     name:'ricky'
+//   };
+//   const secondPerson = person;
+//   console.log(secondPerson) - 'ricky
+//   person.name = 'tony'
+//   console.log(secondPerson) - 'tony'
+//-------- makes a copy of the person
+// const secondPerson =  {
+//   ...person
+// }
+// simple js to double numbers
+// const numbers = [1,2,3];
+
+// const doubleNumArray = numbers.map((chicken) => {
+//   return chicken * 2;
+// })
+
+// console.log(numbers)
+// console.log(doubleNumArray)
